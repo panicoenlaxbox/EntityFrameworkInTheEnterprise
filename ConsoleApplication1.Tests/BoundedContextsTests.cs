865fd176d7b8ee3574499b3db30552ef3d67718b
panicoenlaxbox <panicoenlaxbox@gmail.com>
2015-06-11 00:59:23 +0200

    Initial commit.

---
+++ ConsoleApplication1.Tests/BoundedContextsTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace ConsoleApplication1.Tests
+{
+    public class BoundedContextsTests
+    {
+        [Test]
+        public void CanInsertCustomer()
+        {
+            using (var context = new PrimaryContext())
+            {
+                context.Database.Initialize(false);
+                var customer = new Customer
+                {
+                    Name = "Customer 1",
+                    Code = "C1"
+                };
+                context.Customers.Add(customer);
+                context.SaveChanges();
+                Assert.Pass();
+            }
+        }
+
+        [Test]
+        public void CanRetrieveCustomerReference()
+        {
+            using (var context = new SecondaryContext())
+            {
+                var customerReference = context.Customers.SingleOrDefault(p => p.Name == "Customer 1");
+                Assert.IsNotNull(customerReference);
+            }
+        }
+    }
+}
---
+++ dbo.Customers.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Customers] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (MAX) NOT NULL,
+    [Code] NVARCHAR (50)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
